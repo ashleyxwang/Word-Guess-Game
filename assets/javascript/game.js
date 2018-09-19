e0752abf2634ea19a92ff5959e244fea85b0e6a6
ashleyxwang <ashleywang514@yahoo.com>
2018-09-19 00:54:09 -0500

    another attempt at the Javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,145 @@
-// var artistNames = [
-//     "CLAUDE MONET", "ANDY WARHOL", "JACKSON POLLUCK", "GEORGIA O'KEEFFE", "FRIDA KAHLO", "EDGAR DEGAS", "MARY CASSATT"
-// ];
+var answerWords = [
+    "MONET", "WARHOL", "POLLUCK", "OKEEFFE", "KAHLO", "DEGAS", "CASSATT"
+];
 
 var alphabet = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'
 ];
 
+// takes no arguements and returns a random index into answerWord
+function getRandomIndex() {
+    var randomNumber = Math.random() * answerWords.length;
+    var randomIndex = Math.floor(randomNumber);
+    return randomIndex;
+};
+
+// Create * for each letter in chosen answerWords
+function hiddenForm(word) {
+    var newWord = "";
+    for (let i = 0; i < word.length; i++) {
+        newWord = newWord + "*";
+    }
+    return newWord;
+};
+
+//Index answerWords at randomIndex
+function getRandomWord() {
+    var randomIndex = getRandomIndex();
+    return answerWords[randomIndex];
+};
+
+//ChosenWord in hidden form (with astericks)
+function HiddenWord() {
+    return hiddenForm(chosenWord);
+};
+
+var chosenWord = getRandomWord();
+var hiddenWord = ""
+var correctGuesses = 0;
+var guessesLeft = 6;
+var correctLetters = [];
+var wrongLetters = [];
+
+//Reset Page to start new game
+function reset() {
+    var chosenWord = getRandomWord();
+    var hiddenWord = "";
+    var correctGuesses = 0;
+    var guessesLeft = 6;
+    var correctLetters = [];
+    var wrongLetters = [];
+};
+
+
+// var element = document.createElement("h1").innerHTML;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // returns true if player wins
-function hangmanGame(answerWord) {
-    var word = answerWord.split("");              // Selected word
-    var guesses = [];      // Stored guesses
-    var numWrongGuesses = 0;
-    var space = word.split(" ").length;
-    
-    
+// function hangmanGame(answerWord) {
+//     var word = answerWord.split("");              // Selected word
+//     var guesses = [];      // Stored guesses
+//     var numWrongGuesses = 0;
+//     var space = word.split(" ").length;
 
-    // Call function to display HTML
-    var answerWordNode = document.getElementById("answerWord");
-    for(var letter = 0; letter < word.length; letter++) {
-        // create empty element with the letter
-        // append it or whatever to the dom node 
-        // show spaces
-        // ****-****-****
-        if(word[letter] === " ") {
-            var element = document.createElement("p").innerHTML = "-";
+//     // Call function to display HTML
+//     // Choose random answerWord
+//     var answerWordNode = document.getElementById("answerWord");
+//         function getanswerWord (max, min) {
+//             var answerWord = Math.random.floor() * (7 - 0) + 0;
+//             return (getanswerWord)
+//     for (var letter = 0; letter < word.length; letter++) {
+//         function addElement () { 
+//             // create a new div element 
+//             var newDiv = document.createElement("div");
+//                 newDiv.style = ('font-size: 200px');
+//             var newContent = document.createTextNode("*"); 
+//             newDiv.appendChild(newContent);  
 
-        }
-        var element = document.createElement("p").innerHTML;
-    }
 
-    while(numWrongGuesses <= 6) {
-        var guess = prompt("What's your guess?");
-        // Check guess against stores guesses
-        if (evaluateGuess(guesses, guess)) {
-            console.log("you're a pussy");
-            continue;
-        } 
+//         // create empty element with the letter?
+//         // append it to dom? 
 
-        if (evaluateGuess(word, guess)) {
-            // update html
-            
-        } else {
-            // updateHangman()
-            numWrongGuesses++;
-        }
-    }
-};
+//         if(word[letter] === " ") {
+//             var element = document.createElement("h1").innerHTML = " ";
+//         };
+//         var element = document.createElement("h1").innerHTML;
+//     };
 
-function evaluateGuess(answerWord, guess) {
-    forEach (var i in answerWord) {
-        if (guess === i)
-            return true;
-    }
-    return false;    
-};
+//     // If wrong, display body part, numWrongGuesses++
+//     // If right, display letter
+
+//     while(numWrongGuesses <= 6) {
+//         var guess = prompt("What's your guess?");
+//         // Check guess against stores guesses
+//         if (evaluateGuess(guesses, guess)) {
+//             alert("You guessed that letter already!");
+//             continue;
+//         };
+
+//         if (evaluateGuess(word, guess)) {
+//             // update html
+
+//         } else {
+//             numWrongGuesses++;
+//             //update hangman
+//             var hangman = document.getElementById("hangman")
+//             var newBodyPart = document.createElement("div")
+
+
+//         };
+//     };
+// };
+// function evaluateGuess(answerWord, guess) {
+//     forEach (function(var i  answerWord)) {
+//         if (guess === i)
+//             return true;
+//     }
+//     return false;    
+// };
 
 // function updateGuesses() {
 
 // };
 
-function updateHTML() {
-    
-};
+// function updateHTML() 
 
-
-// If wrong, display body part, numWrongGuesses++
-// If right, display letter
 
 // Track wins :)
